ea7afb8d5197e1f3e9d28d88c44bdf6841992702
telix <telegonicaxx@live.com>
2022-06-09 02:24:45 -0700

    speling is difikult

--- get_all_nodes.py
+++ get_all_nodes.py
@@ -1,52 +1,52 @@
-for dir_ in get_dirs(path):
-    with open(filename) as f:
-    # save graph   
-path = "/home/telix/"
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-dirs = os.listdir( path )
-    print(dir_)
-    return  [f.path for f in os.scandir(path) if f.is_dir()]
-        read the list of urls and set the titlte of the article as node in the graph
-        content = f.readlines()
-        # add node to graph
-    nx.write_gpickle(wiki_graph, "wiki_graph_data.nx")
-# A program which 
-    ''' This would print all the files and directories '''
-    for line in content:
-import sys, os
-    '''
-    '''
-import datetime
-        print("adding node : " + node )
-import re
-    # getting file from passed arguments
-def get_dirs(path):
-    print('reading from file: ' + filename )
-    
-    # opening file 
-    # graph structure for storing the wikipedia nodes
-        print("done")
-    wiki_graph = nx.DiGraph()
-def read_list():
-import sys
-        node =  re.search('(?=\/)/[^/]*$', line).group()
     filename =  sys.argv[1]
         # for every url get the article name with regex
-#!/usr/bin/python
-import pickle
-import networkx as nx
+    # getting file from passed arguments
+def get_dirs(path):
+    # save graph   
+        print("adding node : " + node )
+    nx.write_gpickle(wiki_graph, "wiki_graph_data.nx")
         wiki_graph.add_node(node)
+import pickle
+    # opening file 
+    ''' This would print all the files and directories '''
+import sys, os
+    # graph structure for storing the wikipedia nodes
+def read_list():
+    '''
+    '''
+path = "/home/telix/"
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import networkx as nx
+for dir_ in get_dirs(path):
+    wiki_graph = nx.DiGraph()
+    with open(filename) as f:
         # read line from file
+dirs = os.listdir( path )
+    print('reading from file: ' + filename )
+        print("done")
+#!/usr/bin/python
+        # add node to graph
+        read the list of urls and set the titlte of the article as node in the graph
+        node =  re.search('(?=\/)/[^/]*$', line).group()
+import datetime
+        content = f.readlines()
+# A program which 
+    return  [f.path for f in os.scandir(path) if f.is_dir()]
+    print(dir_)
+    for line in content:
+import sys
+import re
